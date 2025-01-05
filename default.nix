fd884ca6f3a0401ee9744c73106f13cff7e18f10
David Jelenc <david.jelenc@fri.uni-lj.si>
2025-01-05 15:40:30 +0000

    Popravi navodila

---
+++ default.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs> { };
+
+pkgs.mkShell rec {
+  # name = "vp";
+
+  venvDir = "./.venv";
+  buildInputs = [
+    python312Packages.python
+    python312Packages.venvShellHook
+    python312Packages.jupyterlab
+    python312Packages.matplotlib
+    python312Packages.cryptography
+  ];
+
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+    # pip install -r requirements.txt
+    '';
+
+  postShellHook = ''
+    # allow pip to install wheels
+    unset SOURCE_DATE_EPOCH
+    exec zsh
+  '';
+}
